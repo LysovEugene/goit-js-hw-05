c0922b4e2a7f9404a864e88c7c2f36a3f0fbd46f
Eugene Lysov <evg.lysov@gmail.com>
2020-09-21 12:43:47 +0300

    4 task done

--- js/task_3.js
+++ js/task_3.js
@@ -1,34 +1,55 @@
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
+// Write code under this line
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
 
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if (arguments.length >= 3) {
-    accum = initial;
-    i = 0;
+  addItem(newItem) {
+    this.items.push(newItem);
+    return this.items;
   }
-  if (arguments.length === 2) {
-    accum = array[0];
-    i = 1;
+
+  removeItem(itemToRemove) {
+    for (let i = 0; i < this.items.length; i += 1)
+      if (itemToRemove === this.items[i]) {
+        this.items.splice(i, 1);
+      }
   }
-  for (i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-    accum = cb(accum, element);
-  }
-  return accum;
 }
 
-const arr = [1, 2, 3, 4, 5];
+console.log(typeof Storage);
+// 'function'
 
-console.log(reduceArray(arr, add)); // 15
-//console.log(reduceArray(arr, add, 10)); // 25
+const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
 
-console.log(reduceArray(arr, mult)); // 120
-//console.log(reduceArray(arr, mult, 10)); // 1200
+const storage = new Storage(goods);
 
-console.log(reduceArray(arr, sub)); // -13
-//console.log(reduceArray(arr, sub, 10)); // -5
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
--- js/task_4.js
+++ js/task_4.js
@@ -1,30 +1,33 @@
-const account = {
-  owner: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
-  changeDiscount(value) {
-    this.discount = value; // Write code in this line
-  },
-  showOrders() {
-    return this.orders; // Write code in this line
-  },
-  addOrder(cost, order) {
-    this.balance -= cost; // Write code in this line
-    this.orders.push(order); // Write code in this line
-  },
-};
-const copyAccount = Object.assign({}, account);
-copyAccount.orders = [...account.orders];
-// копируем для автотестов ссылочные типы
+// Write code under this line
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+  append(str) {
+    this._value = `${this._value}${str}`;
+  }
+  prepend(str) {
+    this._value = `${str}${this._value}`;
+  }
+  pad(str) {
+    this.append(str);
+    this.prepend(str);
+  }
+}
 
-console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3']
+console.log(typeof StringBuilder);
+// 'function'
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
-console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3', 'order-4']
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+builder.pad('=');
+console.log(builder.value); // '=^.^='
